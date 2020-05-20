0126d828139a1e3af315bde94cea0dc0c3ba2936
NikhilM98 <nikhilmehra998@gmail.com>
2020-05-20 17:30:39 +0530

    Added translation javascript

--- dashboard/public/js/main.js
+++ dashboard/public/js/main.js
@@ -1,3 +1,4 @@
+var lang; // init global variable
 console.log("Hello World");
 
 function setModalContent(content) {
@@ -9,3 +10,68 @@ function setModalContent(content) {
 	document.getElementById('releaseChart').innerText= content.chart;
 	document.getElementById('releaseAppVersion').innerText= content.app_version;
 }
+
+function getLocale() {
+	var languageSelection = document.getElementById('languageSelection');
+	if (languageSelection != null) {
+		if (localStorage.getItem("languageSelection") != null) {
+			lang = localStorage.getItem("languageSelection");
+			languageSelection.value = lang;
+		} else if (languageSelection.selectedIndex != -1) {
+			lang = languageSelection.options[languageSelection.selectedIndex].value;
+			localStorage.setItem("languageSelection", lang);
+		} else {
+			lang = "en";
+		}
+
+		var langList = ["en", "fr", "hi", "es"];
+		var resources = {};
+
+		for (var i=0; i<langList.length; i++) {
+			resources[langList[i]] = {
+				translation: {},
+				response: {}
+			};
+			resources[langList[i]]["response"] = fetch('/public/i18n/'+langList[i]+'.json').then(function(response){
+				return response.json();
+			});
+		}
+
+		Promise.all(langList.map(function (lang) {
+			return resources[lang].response;
+		})).then(function (responses) {
+			for (var i=0; i<responses.length; i++) {
+				resources[langList[i]]["translation"] = responses[i];
+			}
+			initLocale(resources, lang);
+		});
+	}
+}
+
+function initLocale(locale, lang) {
+	moment.locale(lang);
+	i18next.init({
+		lng: lang,
+		fallbackLng: "en",
+		resources: locale
+	}, function(err, t) {
+		jqueryI18next.init(i18next, $, {
+			useOptionsAttr: true
+		});
+		$('#main-content').localize();
+
+		var languageSelection = document.getElementById('languageSelection');
+		languageSelection.onchange = function() {
+			localStorage.setItem("languageSelection", this.value);
+			lang = this.value;
+			moment.locale(lang);
+			i18next.changeLanguage(this.value);
+			$('#main-content').localize();
+		};
+		window.isLocalized = true;
+	});
+}
+
+$(document).ready(function() {
+	getLocale();
+});
