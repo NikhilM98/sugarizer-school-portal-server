bc69824784f789d3e1e273e6e95bbc44c2355e7d
NikhilM98 <nikhilmehra998@gmail.com>
2020-05-24 00:17:11 +0530

    Added regex test cases

--- helper/regexValidate.js
+++ helper/regexValidate.js
@@ -8,11 +8,19 @@ module.exports = function(type) {
 		return /^[a-z0-9]+$/i;
 	case "username":
 		return /^[a-z0-9_-]{3,32}$/i;
+	case "short-name":
+		return /^[a-z0-9]{3,32}$/i;
 	case "email":
 		//All alphanumeric characters case insensitive
 		return /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 	case "language":
 		return /^en$|^es$|^fr$|^hi$/;
+	case "address":
+		return /^[#.0-9a-zA-Z\s,-]+$/;
+	case "number":
+		return /^[0-9]+$/i;
+	case "devices":
+		return /^desktop$|^laptop$|^mobile$|^tablet$/;
 	default:
 		//All alphanumeric characters case insensitive
 		return /^[a-z0-9]+$/i;
