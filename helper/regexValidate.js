ebb6480ac1c49509a7a364c644fac867c1dbe998
ricknjacky <jaikishanbrijwani@gmail.com>
2021-05-24 20:02:36 +0530

    allowing accented characters

--- helper/regexValidate.js
+++ helper/regexValidate.js
@@ -9,8 +9,8 @@ module.exports = function(type) {
 	case "username":
 		return /^[a-z0-9_-]{3,32}$/i;
 	case "short-name":
-		// Alphanumeric. Cannot be purely numeric
-		return /^(?![0-9]*$)[a-z0-9]{3,32}$/i;
+		// Alphanumeric. Cannot be purely numeric allow - and accented characters
+		return /[a-zA-Z\u00C0-\u017F\s.-]+/i;
 	case "email":
 		//All alphanumeric characters case insensitive
 		return /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
