f75b810447867061e50085a1d107dea22050b348
Jaikishan Brijwani <50985033+ricknjacky@users.noreply.github.com>
2021-05-25 16:50:00 +0530

    Allowing accented and minus "-" characters in school name (#30)
    * allowing accented characters
    
    * allowing accented characters and - only in between
    
    * allowing - in school short name
    
    * accented characters in school name update
    
    * final touches to the regex
    
    * updating comments

--- helper/regexValidate.js
+++ helper/regexValidate.js
@@ -1,16 +1,16 @@
 module.exports = function(type) {
 	switch (type) {
 	case "name":
-		//All Alphanumeric characters case insensitive + spaces
-		return /^[a-z0-9 ]+$/i;
+		//All Alphanumeric and accented characters and "-" example: École élémentaire ... Saint-Ouen
+		return /^[a-zA-Z0-9\u00C0-\u017F\s.-]$/;
 	case "pass":
 		//All alphanumeric characters case insensitive
 		return /^[a-z0-9]+$/i;
 	case "username":
 		return /^[a-z0-9_-]{3,32}$/i;
 	case "short-name":
-		// Alphanumeric. Cannot be purely numeric
-		return /^(?![0-9]*$)[a-z0-9]{3,32}$/i;
+		// Alphanumeric. Cannot be purely numeric allow "-"" only in between example:- mso-zola
+		return /^(?![0-9]*$)(?!-)[a-z0-9-]{3,32}(?<!-)$/i;
 	case "email":
 		//All alphanumeric characters case insensitive
 		return /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
