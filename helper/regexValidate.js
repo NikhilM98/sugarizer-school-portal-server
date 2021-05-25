bc70725214156f8079261b971584e2b16a8009af
ricknjacky <jaikishanbrijwani@gmail.com>
2021-05-25 15:01:36 +0530

    final touches to the regex

--- helper/regexValidate.js
+++ helper/regexValidate.js
@@ -2,7 +2,7 @@ module.exports = function(type) {
 	switch (type) {
 	case "name":
 		//All Alphanumeric characters case insensitive + spaces
-		return /^(?!-)[a-zA-Z0-9\u00C0-\u017F\s.-]+(?<!-)$/i;
+		return /^[a-zA-Z0-9\u00C0-\u017F\s.-]$/;
 	case "pass":
 		//All alphanumeric characters case insensitive
 		return /^[a-z0-9]+$/i;
@@ -10,7 +10,7 @@ module.exports = function(type) {
 		return /^[a-z0-9_-]{3,32}$/i;
 	case "short-name":
 		// Alphanumeric. Cannot be purely numeric allow - and accented characters
-		return /^(?![0-9]*$)(?!-)[a-z0-9/-]{3,32}(?<!-)$/i;
+		return /^(?![0-9]*$)(?!-)[a-z0-9-]{3,32}(?<!-)$/i;
 	case "email":
 		//All alphanumeric characters case insensitive
 		return /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
