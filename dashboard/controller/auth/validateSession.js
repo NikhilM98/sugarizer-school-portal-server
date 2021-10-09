7a812861aaf89055edec60896fe813486af7f1e3
Nikhil Mehra <nikhilmehra998@gmail.com>
2021-10-09 20:32:55 +0900

    Prevent token refresh for failed totp verification (#49)

--- dashboard/controller/auth/validateSession.js
+++ dashboard/controller/auth/validateSession.js
@@ -4,9 +4,7 @@
 module.exports = function validateSession(req, res, next) {
 	if (!req.session.user) {
 		return res.redirect('/login');
-	}
-
-	if (req.session.user.partial === true){
+	} else if (req.session.user.partial === true){
 		return res.redirect('/verify2FA');
 	} else if (req.session.user.partial === false && req.session.user.user && req.session.user.user.role) {
 		req.role = req.session.user.user.role;
